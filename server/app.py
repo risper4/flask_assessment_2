5142cfefe56c68d7c2c43f4a6c61ab29f3cf81c7
risper4 <risperwanjikugichia@gmail.com>
2026-07-26 17:17:27 +0300

    Specific workout endpoint

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
 from marshmallow import ValidationError
 
@@ -17,7 +17,8 @@ def get_workouts() :
     try :
         workouts = Workout.query.all()
         
-        body = [{'id' : workout.id, 
+        body = [{
+                    'id' : workout.id, 
                     'date' : workout.date, 
                     'duration_minutes' : workout.duration_minutes, 
                     'notes' : workout.notes}
@@ -28,6 +29,32 @@ def get_workouts() :
     except ValidationError as err :
         print('Valid fields : ', err.valid_data)
         print('Invalid fields : ', err.message)
+
+
+
+@app.route('/workouts/<int:id>', methods=['GET'])
+def get_specific_workout(id)  :
+    try :
+        workout = Workout.query.filter_by(id=id).first()
+
+        if not  workout :
+            body = {f'error': 'Workout {id} not found'}
+            status = 404
+        else :
+            body = {
+                'id' : workout.id, 
+                'date' : workout.date, 
+                'duration_minutes' : workout.duration_minutes, 
+                'notes' : workout.notes
+                }
+            status = 200
+
+        return make_response(body, status)
+
+    except ValidationError as err :
+        print('Valid fields : ', err.valid_data)
+        print('Invalid fields : ', err.messages)
+
 
 
 
